cfd9d16266c016fd646ab9435484be2e4afee28f
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2014-02-05 22:42:44 -0800

    adding files almost done

--- prog2/prog2.cpp
+++ prog2/prog2.cpp
@@ -12,7 +12,7 @@
  #include <vector>
  #include <math.h>
  #include <stdio.h>
- #include <limits.h>
+ #include <float.h>
 
  using namespace std;
  float getDistance(float x1, float y1, float x2, float y2);
@@ -22,6 +22,12 @@
      float x;
      float y;
      Point(float x1, float y1):x(x1),y(y1){}
+ };
+ struct Point2
+ {
+    int x;
+    int y;
+    Point2(int x1, int y1):x(x1),y(y1){}
  };
 
  int main(){
@@ -58,47 +64,65 @@
     float distance;
     for(int i=0; i< points.size();i++){
         for(int j=0; j<points.size();j++){
-            distance = getDistance(points.at(j).x,points.at(j).y,
-                                   points.at(i).x,points.at(i).y
+            distance = getDistance(points.at(i).x,points.at(i).y,
+                                   points.at(j).x,points.at(j).y
             );
-            primsGraph[i][j]=distance;
+            primsGraph[j][i]=distance;
         }
     }
 
    //prims Adjacency matrix complete
    int checkedVertices[totalNumber];
-   int edgeMaxWeight[totalNumber];
+   float edgeMaxWeight[totalNumber];
    bool verticesAdded[totalNumber];
 
    //initially no vertices are mcst &&
    //no edges are minimum
    for(int i=0;i<totalNumber;i++){
         verticesAdded[i]=false;
-        edgeMaxWeight[i]=INT_MAX;
+        edgeMaxWeight[i]=FLT_MAX;
    }
 
    //choose the first edge & initialize explicit
     edgeMaxWeight[0]=0;
     checkedVertices[0]=-1;
-
     for (int i=0; i<totalNumber-1;i++){
-            int min = INT_MAX, index;
-            for (int j=0;j < totalNumber;j++){
-                 if (verticesAdded[j]!=true && edgeMaxWeight[j]<min)
+            float min = FLT_MAX;
+            int index = -1;
+            for (int j=0;j<totalNumber;j++){
+                 if (verticesAdded[j]==false && edgeMaxWeight[j]<=min){
                      min = edgeMaxWeight[j];
                      index = j;
+                 }
             }
-
             verticesAdded[index]=true;
             int edgeIndex = index;
 
-            for (int k = 0; k < totalNumber; k++){
-                 if (primsGraph[k][edgeIndex] && checkedVertices[k] == false && primsGraph[k][edgeIndex] <  edgeMaxWeight[k])
+            for (int k=0;k<totalNumber;k++){
+                 cout<<"primsGraph:"<<primsGraph[edgeIndex][k]<<" edgeMaxWeight[k] "<<edgeMaxWeight[k]<< " checkedVertices "<<checkedVertices <<"\n";
+
+                 if (primsGraph[edgeIndex][k]!=0 && verticesAdded[k] == false && primsGraph[edgeIndex][k]<=edgeMaxWeight[k]){
                                  checkedVertices[k]  = edgeIndex;
-                                 edgeMaxWeight[k] = primsGraph[k][edgeIndex];
+                                 edgeMaxWeight[k] = primsGraph[edgeIndex][k];
+                 }
             }
+            cout<<"\n";
 
     }
+
+       vector<Point2>output;
+       output.push_back(Point2(0,0));
+       for (int i = 1; i < totalNumber; i++){
+          if(checkedVertices[i] < i ){
+            output.push_back(Point2(checkedVertices[i], i));
+          }
+          else{
+             output.push_back(Point2(i,checkedVertices[i]));
+          }
+          
+          printf("%d - %d\n", output.at(i).x, output.at(i).y);
+        }
+
     return 0;
  }
 

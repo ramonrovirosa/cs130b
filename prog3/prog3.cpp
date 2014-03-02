289577192544e9162470e427c9f42c6947c11d19
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2014-03-02 14:04:44 -0800

    matrix bug...

--- prog3/prog3.cpp
+++ prog3/prog3.cpp
@@ -15,6 +15,15 @@
 
  using namespace std;
 
+//min(a,b, leftMatrix, topMatrix, diagonalMatrix, x, y, match, change, del)
+ double min(int a, int b,
+            double leftMatrix, double topMatrix, double diagonalMatrix,
+            double match, double change, double del){
+
+    return 5.0;
+ }
+
+
 
  int main(int argc, char* argv[]){
     string x="GACGGATTAG";
@@ -23,9 +32,34 @@
     int i = x.length();
     int j = y.length();
 
-    int Matrix[i][j];
+    double match=2;
+    double change=-.5;
+    double del=-1;
 
+    for(int a=0;a<i;a++){
+        Matrix[a][0] = 1.0;
+    }
+    for(int b=0;b<j;b++){
+        Matrix[0][b] = 1.0;
+    }
+    cout<<"Matrix[0][10] "<<Matrix[0][10]<<endl;
+    Matrix[0][0]=(x[0]==y[0])?2.0:-0.5;
 
+   for (int a = 1; a < j; a++){
+       for(int b=1; b<i;b++){
+            double leftMatrix=Matrix[a-1][b];
+            double topMatrix=Matrix[a][b-1];
+            double diagonalMatrix=Matrix[a-1][b-1];
+            Matrix[a][b] = min(a,b, leftMatrix, topMatrix, diagonalMatrix, match, change, del);
+        }
+    }
+
+    for (int a=0; a< j;a++){
+            for(int b=0; b<i;b++){
+                cout<<Matrix[a][b]<< " ";
+            }
+            cout<<endl;
+        }
 
     return 0;
  }

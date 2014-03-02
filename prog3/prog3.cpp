649d763967bcc32a02e59b43d44cb49834d96c63
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2014-03-01 19:12:18 -0800

    adding prog3

---
+++ prog3/prog3.cpp
@@ -0,0 +1,21 @@
+/*
+    Ramon Rovirosa
+    CS130B
+    Prog3
+ */
+
+ #include <cstdlib>
+ #include <iostream>
+ #include <fstream>
+ #include <sstream>
+ #include <vector>
+ #include <math.h>
+ #include <stdio.h>
+ #include <float.h>
+
+ using namespace std;
+
+
+ int main(){
+   return 0;
+ }

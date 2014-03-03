3f4b6ffa76d3f7493e3b07a7a681d984a9337a7e
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2014-03-02 16:58:59 -0800

    passes first test case

--- prog3/prog3.cpp
+++ prog3/prog3.cpp
@@ -28,11 +28,12 @@ double max(double left, double top, double diagonal){
             double match, double change, double del){
 
     double sim;
-    cout<<"x["<<a<<"] "<<x[a]<<" y["<<b<<"] "<<y[b]<<endl;
-    if(x[a] == y[b])
+//    cout<<"x["<<a<<"] "<<x[a]<<" y["<<b<<"] "<<y[b]<<endl;
+    if(x[a-1] == y[b-1]){
         sim=match;
-    else
+    }else{
         sim=change;
+    }
 
     double left= leftMatrix + del;
     double top = topMatrix + del;
@@ -45,33 +46,31 @@ double max(double left, double top, double diagonal){
 
 
  int main(int argc, char* argv[]){
-    string x="GACGGATTAG";
-    string y="GATCGGAATAG";
+    string x="ACACACTA";
+    string y="AGCACACA";
 
     int i = x.length();
     int j = y.length();
 
-//    double match=2.0;
-//    double change=-.5;
-//    double del=-1.0;
+    double match=2.0;
+    double change=-.5;
+    double del=-1.0;
 
-    double match=1;
-    double change=-1.0;
-    double del=-2.0;
+//    double match=1;
+//    double change=-1.0;
+//    double del=-2.0;
 
-    double Matrix[i][j];
+    double Matrix[i+1][j+1];
 
-    for(int a=0;a<i;a++){
+    for(int a=0;a<=i;a++){
         Matrix[a][0] = a*del;
     }
-    for(int b=0;b<j;b++){
+    for(int b=0;b<=j;b++){
         Matrix[0][b] = b*del;
     }
-    Matrix[0][0]=(x[0]==y[0])?match:change;
-
    //b=x, a=y
-   for (int a = 1; a < j; a++){
-       for(int b=1; b<i;b++){
+   for (int a = 1; a<= j; a++){
+       for(int b=1;b<=i;b++){
             double leftMatrix=Matrix[b-1][a];
             double topMatrix=Matrix[b][a-1];
             double diagonalMatrix=Matrix[b-1][a-1];
@@ -80,12 +79,19 @@ double max(double left, double top, double diagonal){
         }
     }
 
-    for (int a=0; a< j;a++){
-            for(int b=0; b<i;b++){
-                cout<<Matrix[b][a]<< " ";
+    for (int a=0; a<=j;a++){
+            for(int b=0; b<=i;b++){
+                if(Matrix[b][a] < -9)
+                    cout<<Matrix[b][a]<< " ";
+                else if(Matrix[b][a] < 0 || Matrix[b][a] > 9)
+                    cout<<""<<Matrix[b][a]<< "  ";
+                else
+                    cout<<" "<<Matrix[b][a]<< "  ";
             }
             cout<<endl;
         }
+
+    cout<< Matrix[i][j]<<endl;
 
     return 0;
  }
